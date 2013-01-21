1bffca14ca704a90dd705f01e34f347abdd42bb2
Mikhail Khomutetskiy <khomutetskiy@gmail.com>
2013-01-21 11:10:39 +0700

    Remove task action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,26 @@
 class TasksController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_task, only: [:complete, :destroy]
 
   def index
-    @tasks = Task.where(user_id: current_user.id)
+    @tasks = Task.where(user_id: current_user.id).order('id DESC')
   end
 
   def create
     @task = current_user.tasks.create(params[:task])
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  def complete
+    @task.complete!
+  end
+
+  protected
+
+  def find_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Todola::Application.routes.draw do
   devise_for :users
 
-  resources :tasks
+  resources :tasks do
+    member do
+      post :complete
+    end
+  end
 
   root to: 'tasks#index'
 end

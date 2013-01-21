62c49d407a054998d1c43696b85fd6d385c75e48
Mikhail Khomutetskiy <khomutetskiy@gmail.com>
2013-01-21 11:43:52 +0700

    Show priority labels

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,16 @@
 module TasksHelper
+  TASK_PRIORITY_TO_CLASS = {
+    high: :important,
+    medium: :warning,
+    low: :info
+  }
+
+  def task_priority_label(task)
+    if task.priority
+      text = t(task.priority_sym, scope: 'task.priorities')
+      label_class = TASK_PRIORITY_TO_CLASS[task.priority_sym]
+
+      %{<span class="label label-#{label_class}">#{text}</span>}.html_safe
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,16 @@
 class Task < ActiveRecord::Base
+  # 1 - high
+  # 2 - medium
+  # 3 - low
+  PRIORITIES = (1..3).to_a
+
   attr_accessible :name, :priority, :due_date
 
   belongs_to :user
 
   validates :name, :state, presence: true
   validates :name, length: { maximum: 255 }
+  validates :priority, inclusion: { in: PRIORITIES }
 
   state_machine :state, initial: :pending  do
     state :pending
@@ -12,6 +18,14 @@ class Task < ActiveRecord::Base
 
     event :complete do
       transition pending: :completed
+    end
+  end
+
+  def priority_sym
+    case priority
+    when 1 then :high
+    when 2 then :medium
+    when 3 then :low
     end
   end
 end
